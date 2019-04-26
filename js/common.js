f532e936b14967f87e66c94eb09fdaac73a360c3
Kordai <40060837+Kordai@users.noreply.github.com>
2019-04-26 11:36:50 +0600

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
 	
-	$(".top_banner").mousemove(function (e) {
-		let $wit = ($(".top_banner").width() / 2) - 350;
+	$(".top_banner1").mousemove(function (e) {
+		let $wit = ($(".top_banner1").width() / 2) - 350;
 		console.log($wit);
 		var moveX = (e.pageX * 1/30) + $wit;
 		var moveY = (e.pageY * 1/30);
